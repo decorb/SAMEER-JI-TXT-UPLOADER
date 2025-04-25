34152ebfdc89cdf4842f40d3138482ea61cbca1e
decorb <decorb18@gmail.com>
2025-04-25 10:46:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ async def show_random_emojis(message):
 OWNER_ID = 7615363859 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
-SUDO_USERS = [5500536251]
+SUDO_USERS = [5500536251,7080838404]
 
 AUTH_CHANNEL = -1002384727235
 
